9a8998ac1f4bac4a3f71ee1f8ceed566d8015bf4
Lia Porto <liabarcellos28@gmail.com>
2021-08-16 12:10:25 -0300

    feature: implement footer (without responsivity)

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+import escolaDeDadosImg from "../../../public/img/logo-ed01-branca.png";
+import okbrImg from "../../../public/img/logo-okbr-branca-alt.png";
+
+import styles from "./styles.module.scss";
+
+const Footer = ({ handleModalOpen }) => {
+  return (
+    <footer className={styles.pageFooter}>
+      <div className={styles.mainRow}>
+        <div className={styles.linksContainer}>
+          <a
+            className={styles.footerLink}
+            href="#"
+            onClick={() => handleModalOpen("aboutPage")}
+          >
+            O que é a Caixa de Ferramentas?
+          </a>
+          <a
+            className={styles.footerLink}
+            href="#"
+            onClick={() => handleModalOpen("howTo")}
+          >
+            Quero colaborar
+          </a>
+          <a
+            className={styles.footerLink}
+            href="https://premio.jornalismodedados.org/"
+          >
+            Prêmio Cláudio Weber Abramo
+          </a>
+          <a
+            className={styles.footerLink}
+            href="https://forum.jornalismodedados.org/"
+          >
+            Fórum Jornalismo de Dados
+          </a>
+        </div>
+        <div className={styles.madeByContainer}>
+          <span className="mb-4">Realização</span>
+          <div className={styles.logoContainer}>
+            <a className={styles.logoImgLink} href="https://escoladedados.org/">
+              <img
+                className={styles.logoImg}
+                src={escolaDeDadosImg}
+                alt="Logo da Escola de Dados"
+              />
+            </a>
+            <a className={styles.logoImgLink} href="https://www.ok.org.br/">
+              <img
+                className={styles.logoImg}
+                src={okbrImg}
+                alt="Logo da Open Knowledge Brasil"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.creditsRow}>
+        <span className={`${styles.creditsSpan} mb-2`}>
+          Desenvolvimento:{" "}
+          <a href="https://liaporto.github.io/pt-br/">Lia Porto</a>
+        </span>
+        <span className={styles.creditsSpan}>
+          Nosso conteúdo está disponível sob a licença{" "}
+          <a href="https://creativecommons.org/licenses/by/4.0/">
+            Creative Commons Atribuição 4.0 Internacional
+          </a>
+          , e pode ser compartilhado e reutilizado para trabalhos derivados,
+          desde que citada a fonte.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,65 +1,80 @@
 import { useState, useEffect } from "react";
-import yaml from 'js-yaml'
+import yaml from "js-yaml";
 
 import Head from "next/head";
-import { Modal } from "react-bootstrap";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import InfoModal from "../components/InfoModal";
 
-import styles from '../styles/Home.module.scss';
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  
   const [toolsData, setToolsData] = useState([]);
 
   const [showHowToModal, setShowHowToModal] = useState(false);
   const [showAboutPageModal, setShowAboutPageModal] = useState(false);
 
-  const getUpdatedData = async() => {
-    const updatedData = await fetch('/toolkit_ddj/data/tools.yml').then(res => res.text()).then(data => yaml.load(data));
+  const getUpdatedData = async () => {
+    const updatedData = await fetch("/toolkit_ddj/data/tools.yml")
+      .then((res) => res.text())
+      .then((data) => yaml.load(data));
     console.log(updatedData);
     setToolsData([...updatedData]);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getUpdatedData();
-  }, [])
+  }, []);
 
   const handleModalClose = () => {
-    if(showHowToModal){
+    if (showHowToModal) {
       setShowHowToModal(false);
     } else {
       setShowAboutPageModal(false);
     }
-  }
+  };
 
   const handleModalOpen = (modalName) => {
-    if(modalName==="howTo"){
+    if (modalName === "howTo") {
       setShowHowToModal(true);
     } else {
       setShowAboutPageModal(true);
     }
-  }
+  };
 
   return (
     <div>
       <Head>
         <title>Caixa de Ferramentas | Jornalismo de Dados</title>
-        <meta name="description" content="Explore mais de 140 ferramentas para jornalistas de dados e colabore para aumentar a base." />
+        <meta
+          name="description"
+          content="Explore mais de 140 ferramentas para jornalistas de dados e colabore para aumentar a base."
+        />
         <link rel="icon" href="/favicon.ico" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <Header handleModalOpen={handleModalOpen}/>
+      <Header handleModalOpen={handleModalOpen} />
 
-      <InfoModal showModal={showHowToModal} onHide={handleModalClose} type="howTo"/>
-      <InfoModal showModal={showAboutPageModal} onHide={handleModalClose} type="aboutPage"/>
+      <InfoModal
+        showModal={showHowToModal}
+        onHide={handleModalClose}
+        type="howTo"
+      />
+      <InfoModal
+        showModal={showAboutPageModal}
+        onHide={handleModalClose}
+        type="aboutPage"
+      />
 
-      <main>
+      {/* <main>
         <div>
           {toolsData.length > 0 ?
           toolsData.map((tool, index) => {
@@ -89,9 +104,11 @@ export default function Home() {
           })
           :
           <div>Loading...</div>
-          }
+          } 
         </div>
-      </main>
+      </main>*/}
+
+      <Footer handleModalOpen={handleModalOpen} />
     </div>
   );
 }
